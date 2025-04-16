23cb4f85b789823f409926e224266465419d0634
Renan Aprigio Dias de Moura <renan.dias3158@gmail.com>
2025-04-16 08:49:19 -0300

    Create codigo das questões-1°exercicio.py

---
+++ codigo das questões-1°exercicio.py
@@ -0,0 +1,81 @@
+"""questao 1:
+  insira_texto = input("insira o seu texto aqui:");
+  print(insira_texto + "o texto é belo(a)!");
+"""
+""" questao 2:
+  item1 = int(input("insira o numero:"));
+  item2 = int(input("insira o segundo numero:"));
+  soma = print(item1 + item2);
+"""
+"""questao 3:
+  preço = float(input("insira o preço:"));
+  quant = int(input("insira a quantidade:"));
+  total = print(preço*quant);
+"""
+""" questao 4:
+  preço = float(input("insira o preço:"));
+  desconto = float(input("insira o desconto:"));
+  total = print(preço*desconto);
+"""
+"""questao 5:
+  salario = float(input("qual o salario:"));
+  aumento = float(input("qual o aumento:"));
+  resultado = print(salario*aumento);
+"""
+"""questao 6:
+  int(print(28*43));
+"""
+"""questao 7:
+  valor1 = float(input("o primeiro valor:");
+  valor2 = float(input("o segundo valor:");
+  valor3 = float(input("o terceiro valor:");
+  media = print(valor1 + valor2 + valor3 / 3);
+"""
+"""questao 8:
+  valor = int(input("insira o numero"));
+  sucessor - print(valor + 1);
+  antecessor = print(valor - 1);
+"""
+"""questao 9:
+  valor1 = float(input("o primeiro valor:");
+  valor2 = float(input("o segundo valor:");
+  valor3 = float(input("o terceiro valor:");
+  valor4 = float(input("o terceiro valor:"));
+  mediap = print(valor1*1 + valor2*2 + valor3*3 + valor4*4 / 10);
+"""
+"""questao 10:
+  aplicacao = float(input("insira o valor"));
+  reajuste = print(aplicacao*2.5);
+"""
+"""questao 11:
+  valor = int(input("insira o valor:"));
+  taxa = int(input("insira a taxa:"));
+  tempo = int(input("insira o tempo:"));
+  prestacao = print(valor*(taxa/100)*tempo);
+"""
+"""questao 12:
+  valor1 = int(input("o primeiro valor:");
+  valor2 = int(input("o segundo valor:");
+  soma = print(valor1 + valor2);
+  subtracao = print(valor1 - valor2);
+  multiplicao = print(valor1*valor2);
+  divisao = print(valor1//valor2);
+  resto = print(valor1 % valor2);
+"""
+"""questao 13:
+  base = int(print("insira o valor da base:"));
+  altura = int(input("insira o valor da altura:"));
+  perimetro = print(base*2 + altura*2);
+  area = print(base*altura);
+"""
+"""questao 14:
+    base = int(print("insira o valor da base:"));
+  altura = int(input("insira o valor da altura:"));
+  area = print(base*altura / 2);
+"""
+"""questao 15:
+  pi = 3.14
+  raio = float(input("insira o raio"));
+  perimetro = print(2*pi*raio);
+  area = print(pi*raio**2);
+"""
