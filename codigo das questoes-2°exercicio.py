951f8cbaaaa582f72f17ae514de9f4f3d8d815fd
Renan Aprigio Dias de Moura <renan.dias3158@gmail.com>
2025-04-16 08:45:31 -0300

    Create codigo das questoes-2°exercicio.py
    rfuufudsf

---
+++ codigo das questoes-2°exercicio.py
@@ -0,0 +1,62 @@
+"""questao 1 """
+"""
+numero = input("insira um numero\n-")
+int(numero)
+calculo = numero % 2
+if calculo != 0:
+    print("o numero é impar!")
+elif calculo == 0:
+    print("o numero é par!")
+#end file;
+"""
+"""questao 2"""
+"""
+media1 = input("insira a sua primeira nota\n-")
+float(media1)
+media2 = input("insira a sua segunda nota\n-")
+float(media2)
+media3 = input("insira a sua terceira nota\n-")
+float(media3)
+calc = (media1 + media2 + media3 / 3)
+if calc < 4:
+    print("voce esta reprovado!")
+elif calc >= 4 and calc < 7:
+    print("voce terá que fazer a prova final!")
+elif calc > 7:
+    print("voce esta aprovado!")
+"""
+"""questao 3"""
+"""
+idade = input("qual a sua idade, nadador?")
+int(idade)
+if idade >= 5 and idade <= 7:
+    print("a sua categoria é a Junior!")
+elif idade >= 8 and idade <= 12:
+    print("a sua categoria é a Infantil!")
+elif idade >= 13 and idade <= 18:
+    print("a sua categoria é a Pré!")
+elif idade >18:
+    print("a sua categoria é a Avançada!")
+"""
+"""questao 4"""
+'''
+valor1 = input("qual o primeiro valor?\n-->")
+float(valor1)
+operador = input("escolha um operador:\nsoma = 1\nsubtracao = 2\ndivisao = 3\nmultiplicao = 4 \n-->")
+int(operador)
+valor2 = input("qual o segundo valor\n-->")
+float(valor2)
+if operador == 1:
+    print(valor1 + valor2)
+elif operador == 2:
+    print(valor1 - valor2)
+elif operador == 3:
+    divisao = print(valor1 / valor2)
+    if divisao == 0:
+        print("valor INVALIDO!")
+elif operador == 4:
+    print(valor1*valor2)
+elif operador != (1,2,3,4):
+    print("operador INVALIDO")
+'''
+'''questao 5'''
